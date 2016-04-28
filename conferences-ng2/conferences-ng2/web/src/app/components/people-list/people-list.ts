ab6e32bed88ced281611c29ab0712fbe26609f02
Mathieu Savy <mathieu.savy@gmail.com>
2016-04-28 16:48:22 +0200

    People add form

--- conferences-ng2/conferences-ng2/web/src/app/components/people-list/people-list.ts
+++ conferences-ng2/conferences-ng2/web/src/app/components/people-list/people-list.ts
@@ -10,19 +10,42 @@ export class PeopleList implements OnInit {
   
   public peopleList: any[];
   public currentPeople: any;
+  public newPerson: string;
+
   
   constructor(private _wakandaService: WakandaService) {
     this.peopleList = [];
     this.currentPeople = null;
+    this.newPerson = null;
   }
   
   public clickOnPeople(p) {
     this.currentPeople = p;
   }
   
+    
+  public addPeople() {
+    if (this.newPerson) {
+      this._wakandaService.catalog.then(c => {
+        let person = c['Person'].create({
+          name: this.newPerson
+        });
+        
+        person.save().then(() => {
+          this.newPerson = null;
+          this.loadPersons();
+        });
+      });
+    }
+  }
+  
   ngOnInit() {
+    this.loadPersons();
+  }
+  
+  loadPersons() {
     this._wakandaService.catalog.then(c => {
-      c['Person'].query({select: 'conferences'}).then(collection => {
+      c['Person'].query({select: 'conferences', orderBy: 'name'}).then(collection => {
         this.peopleList = collection.entities; 
       });
     });
