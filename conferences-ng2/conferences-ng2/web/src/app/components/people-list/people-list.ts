b7ce3c0d380df6fd4885b1f6910a558fa9386e21
Mathieu Savy <mathieu.savy@gmail.com>
2016-04-28 17:52:42 +0200

    registering people to conf. WIP: bug when changing value on select

--- conferences-ng2/conferences-ng2/web/src/app/components/people-list/people-list.ts
+++ conferences-ng2/conferences-ng2/web/src/app/components/people-list/people-list.ts
@@ -11,16 +11,22 @@ export class PeopleList implements OnInit {
   public peopleList: any[];
   public currentPeople: any;
   public newPerson: string;
-
+  private conferences: any[];
+  public currentPeopleAvailableConf: any[];
+  public currentPeopleConfToRegister: any;
   
   constructor(private _wakandaService: WakandaService) {
     this.peopleList = [];
     this.currentPeople = null;
     this.newPerson = null;
+    this.conferences = [];
+    this.currentPeopleAvailableConf = [];
+    this.currentPeopleConfToRegister = null;
   }
   
   public clickOnPeople(p) {
     this.currentPeople = p;
+    this.getAvailableConferences(p);
   }
   
     
@@ -43,11 +49,62 @@ export class PeopleList implements OnInit {
     this.loadPersons();
   }
   
-  loadPersons() {
+  
+  private loadPersons() {
     this._wakandaService.catalog.then(c => {
       c['Person'].query({select: 'conferences', orderBy: 'name'}).then(collection => {
         this.peopleList = collection.entities; 
       });
     });
+  }
+  
+  private getAllConferences() {
+    if (this.conferences.length === 0) {
+      return this._wakandaService.catalog.then(c => {
+        return c['Conference'].query({orderBy: 'name'}).then(col => {
+          this.conferences = col.entities;
+          return col.entities 
+        });
+      });
+    }
+    
+    return Promise.resolve(this.conferences);
+  }
+  
+  private getAvailableConferences(people: any) {
+    console.log('1423');
+    
+    this.getAllConferences().then(conferences => {
+      this.currentPeopleAvailableConf = conferences.filter((conf: any) => {
+        return !this.isRegisterToConf(people, conf.name);
+      });
+      this.currentPeopleConfToRegister = this.currentPeopleAvailableConf[0];
+    });
+    
+  }
+  
+  private isRegisterToConf(people: any, confName: string) {
+    for (let c of people.conferences.entities) {
+      if (c.name === confName) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  public registerToConference() {
+    if (this.currentPeopleConfToRegister) {
+      this._wakandaService.catalog.then(c => {
+        let attended = c['Attended'].create({
+          person: this.currentPeople,
+          conference: this.currentPeopleConfToRegister
+        });
+        attended.save().then(() => {
+          this.currentPeople.fetch({select: 'conferences'}).then(() => {
+            this.getAvailableConferences(this.currentPeople);
+          });
+        });
+      });
+    }
   }
 }
