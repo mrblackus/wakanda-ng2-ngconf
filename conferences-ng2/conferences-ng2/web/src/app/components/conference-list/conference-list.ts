314deabea1bf56ffe1d80b37ae4c2419fc82af66
Mathieu Savy <mathieu.savy@gmail.com>
2016-04-29 14:03:07 +0200

    new conference form done

--- conferences-ng2/conferences-ng2/web/src/app/components/conference-list/conference-list.ts
+++ conferences-ng2/conferences-ng2/web/src/app/components/conference-list/conference-list.ts
@@ -10,21 +10,39 @@ export class ConferenceList implements OnInit {
   
   public conferences: any[];
   public currentConf: any;
+  public newConference: string;
   
   constructor(private _wakandaService: WakandaService) {
     this.conferences = [];
     this.currentConf = null;
+    this.newConference = null;
   }
   
   public clickOnConf(c) {
     this.currentConf = c;
   }
   
-  ngOnInit() {
+  public addConference() {
+    if (this.newConference) {
+      this._wakandaService.catalog.then(c => {
+        let conference = c['Conference'].create({name: this.newConference});
+        conference.save().then(() => {
+          this.newConference = null;
+          this.loadConferences();
+        });
+      });
+    }
+  }
+  
+  private loadConferences() {
     this._wakandaService.catalog.then(c => {
       c['Conference'].query({select: 'people'}).then(collection => {
           this.conferences = collection.entities;
       });
     });
+  }
+  
+  ngOnInit() {
+    this.loadConferences();
   }
 }
